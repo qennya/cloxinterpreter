379fd7334b69a7e63cabd686776e6f98af282932
Kenia <velascokenia22@gmail.com>
2026-03-24 15:50:37 -0700

    Challenge 16.1

--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -80,8 +81,7 @@ push(a op b); \
 }
 
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
