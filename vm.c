d9d79898e1e1f1e2b98ff20c3b78a25f1d8ce7fa
Kenia <velascokenia22@gmail.com>
2026-04-12 19:01:16 -0700

    Chapter 21 COMPLETE

--- vm.c
+++ vm.c
@@ -132,6 +132,17 @@ push(valueType(a op b)); \
                 pop();
                 break;
             }
+
+            case OP_SET_GLOBAL: {
+                ObjString* name = READ_STRING();
+                if (tableSet(&vm.globals, name, peek(0))) {
+                    tableDelete(&vm.globals, name);
+                    runtimeError("Undefined variable '%s'.", name->chars);
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                break;
+            }
+
             case OP_EQUAL: {
                 Value b = pop();
                 Value a = pop();
