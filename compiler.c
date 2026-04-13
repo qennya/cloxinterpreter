449500b4e442750db24939ef6e17b6e2115e06fe
Kenia <velascokenia22@gmail.com>
2026-04-12 20:50:50 -0700

    Chpater 22.2 Done

--- compiler.c
+++ compiler.c
@@ -155,6 +155,14 @@ static void endCompiler() {
 #endif
 }
 
+static void beginScope() {
+    current->scopeDepth++;
+}
+
+static void endScope() {
+    current->scopeDepth--;
+}
+
 static void expression();
 static void statement();
 static void declaration();
@@ -211,6 +219,14 @@ static void grouping(bool canAssign) {
 
 static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void block() {
+    while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+        declaration();
+    }
+
+    consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
 }
 
 static void varDeclaration() {
@@ -276,7 +292,12 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
-    } else {
+    } else if (match(TOKEN_LEFT_BRACE)) {
+        beginScope();
+        block();
+        endScope();
+    }
+    else {
         expressionStatement();
     }
 }
